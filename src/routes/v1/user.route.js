ce13a59334d1ae2a9bd798f37cd8081bb89bc352
Akshaypareek01 <akshay96102@gmail.com>
2025-03-27 15:17:33 +0530

    working

--- src/routes/v1/user.route.js
+++ src/routes/v1/user.route.js
@@ -5,41 +5,10 @@ import * as userValidation from '../../validations/user.validation.js';
 import * as userController from '../../controllers/user.controller.js';
 import { otpRateLimiter } from '../../middlewares/otpRateLimit.js';
 import jwt from 'jsonwebtoken';
+import authMiddleware from '../../middlewares/auth.middlewares.js';
 const router = express.Router();
 
-const authMiddleware = (requiredRole = null) => async (req, res, next) => {
-    try {
-      const authHeader = req.headers.authorization;
-  
-      if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        return res.status(401).json({ message: 'Authentication required' });
-      }
-  
-      const token = authHeader.split(' ')[1];
-  
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  
-      // Attach user data to request
-      req.userId = decoded.userId;
-      req.email = decoded.email;
-      req.role = decoded.role;  // Attach role
-  
-      // Check for admin access if required
-      if (requiredRole && req.role !== requiredRole) {
-        return res.status(403).json({ message: 'Forbidden: Admin access required' });
-      }
-  
-      next();
-    } catch (error) {
-      if (error.name === 'TokenExpiredError') {
-        return res.status(401).json({ message: 'Token expired, please login again' });
-      }
-      if (error.name === 'JsonWebTokenError') {
-        return res.status(401).json({ message: 'Invalid token' });
-      }
-      return res.status(500).json({ message: 'Internal server error' });
-    }
-  };
+
 
 
 /*********************/
@@ -55,7 +24,7 @@ router.post('/login', validate(userValidation.login), userController.loginUser);
 
 router.get('/all-users', userController.getAllUsers);
 
-router.get('/get-user/:userId',authMiddleware(),validate(userValidation.getUser) ,userController.getUser);
+router.get('/get-user/:userId',authMiddleware('admin'),validate(userValidation.getUser) ,userController.getUser);
 
 router.get('/active-blockchain/:userId',authMiddleware(),validate(userValidation.getUser) ,userController.getActiveBlockchain);
 
