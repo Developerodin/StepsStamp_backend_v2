508e1f3b2674d5f314a270ab8d714ad7f1fe1c7a
Abhishekanjana <abhishekanjana85@gmail.com>
2025-04-03 17:34:32 +0530

    working

--- src/routes/v1/notifications.route.js
+++ src/routes/v1/notifications.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import notificationController from '../../controllers/notification.controller.js';
+import authMiddleware from '../../middlewares/auth.middlewares.js';
 
 const router = express.Router();
 
 // Route to fetch all notifications for a user
-router.get('/:userId', notificationController.getNotifications);
-router.post('/update', notificationController.updateNotifications);
+router.get('/:userId',authMiddleware(), notificationController.getNotifications);
+router.post('/update',authMiddleware(), notificationController.updateNotifications);
 
 
 
